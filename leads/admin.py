904cc50c76e1e7b6e16324e2acc2d7fd40a5ce1b
Arthur Kozhemyakin <trable228@icloud.com>
2026-06-24 11:26:40 +0300

    Add leads list page with styling and admin registration

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Lead
+
+
+@admin.register(Lead)
+class LeadAdmin(admin.ModelAdmin):
+    list_display = ['name', 'phone', 'email', 'course', 'source', 'created_at']
+    list_filter = ['source', 'course', 'created_at']
+    search_fields = ['name', 'phone', 'email']
+    ordering = ['-created_at']
+    readonly_fields = ['created_at']
