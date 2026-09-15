c5542a52619062b062f17e1705985dc00bdf1246
Arthur Kozhemyakin <trable228@icloud.com>
2026-09-15 12:04:30 +0300

    new minimalistic clients list page

--- clients/views.py
+++ clients/views.py
@@ -1,17 +1,56 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import JsonResponse
 from django.contrib.admin.views.decorators import staff_member_required
-from core.models import Client
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+
+from core.models import Client, Lesson
 
 
 @staff_member_required
 def client_list(request):
     clients = Client.objects.all().order_by('-id')
+
+    search = request.GET.get('search', '').strip()
+    status = request.GET.get('status', '').strip()
+
+    if search:
+        clients = clients.filter(name__icontains=search)
+
+    if status:
+        clients = clients.filter(lifecycle_status=status)
+
     status_choices = Client._meta.get_field('lifecycle_status').choices
 
     return render(request, 'clients/client_list.html', {
         'clients': clients,
         'status_choices': status_choices,
+        'search': search,
+        'selected_status': status,
+    })
+
+
+@staff_member_required
+def client_detail(request, client_id):
+    client = get_object_or_404(Client, pk=client_id)
+
+    lessons = Lesson.objects.filter(
+        student__client=client
+    ).order_by('-date', '-time')
+
+    last_lesson = lessons.filter(
+        status='completed'
+    ).first()
+
+    next_lesson = lessons.filter(
+        status='scheduled'
+    ).order_by('date', 'time').first()
+
+    lesson_history = lessons[:20]
+
+    return render(request, 'clients/client_detail.html', {
+        'client': client,
+        'last_lesson': last_lesson,
+        'next_lesson': next_lesson,
+        'lesson_history': lesson_history,
     })
 
 
@@ -24,6 +63,7 @@ def update_client_status(request, client_id):
     new_status = request.POST.get('status')
 
     valid_statuses = dict(Client._meta.get_field('lifecycle_status').choices)
+
     if new_status in valid_statuses:
         client.lifecycle_status = new_status
         client.save(update_fields=['lifecycle_status'])
